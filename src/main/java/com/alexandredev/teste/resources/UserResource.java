2069fbac2a2c28f73ab7a0b6e1aa482dea505277
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2023-01-31 11:45:29 -0300

    inserção de usuario com POST

--- src/main/java/com/alexandredev/teste/resources/UserResource.java
+++ src/main/java/com/alexandredev/teste/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.alexandredev.teste.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alexandredev.teste.DTO.UserDTO;
 import com.alexandredev.teste.entities.User;
@@ -34,11 +37,13 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
-	/*
-	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj) {
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
-	*/
+	
 }
--- src/main/java/com/alexandredev/teste/services/UserService.java
+++ src/main/java/com/alexandredev/teste/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alexandredev.teste.DTO.UserDTO;
 import com.alexandredev.teste.entities.User;
 import com.alexandredev.teste.repository.UserRepository;
 import com.alexandredev.teste.services.exception.ObjectNotFoundException;
@@ -25,9 +26,12 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 	
-	/*
+	
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	*/
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getBirthDate());
+	}
 }
